ad1dc7bbc73a9655ad67c9c598396f573bbf3591
luizgfc <luizgfc@proton.me>
2025-04-14 06:53:58 -0300

    nvim: git blame and color adjusmtents

---
+++ nvim/lua/plugins/messenger.lua
@@ -0,0 +1,9 @@
+return {
+  -- "lsig/messenger.nvim",
+  "rhysd/git-messenger.vim",
+  -- opts = {
+  --   -- your configuration comes here
+  --   -- or leave it empty to use the default settings
+  --   -- refer to the configuration section below
+  -- }
+}
