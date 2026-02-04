aa01279b4d2797133da00e99f1a2050835699af6
luiz <luizgfc@proton.me>
2026-02-04 12:09:54 -0300

    fix workflow with oil.nvim

--- nvim/lua/setup/plugin/oil.lua
+++ nvim/lua/setup/plugin/oil.lua
@@ -1,1 +1,12 @@
-require("oil").setup()
+require("oil").setup({
+  keymaps = {
+    ["<C-p>"] = false,
+    ["gp"] = "actions.preview",
+  },
+  watch_for_changes = true,
+  view_options = {
+    show_hidden = true
+  }
+})
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
